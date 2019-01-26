6965bebd1723ff7aa72562b3d82037d6ebb60e2a
sin-ad1973 <sin.ad1973@gmail.com>
2019-01-26 12:35:09 +0900

    test

--- src/test/java/jp/gihyo/jenkinsbook/action/SampleActionTest.java
+++ src/test/java/jp/gihyo/jenkinsbook/action/SampleActionTest.java
@@ -77,6 +77,21 @@ public class SampleActionTest {
 	}
 
 	@Test
+	public void testCheckError4() {
+		HttpServletRequest request = createMock(HttpServletRequest.class);
+		SampleAction action = new SampleAction();
+
+		expect(request.getParameter("FirstName")).andReturn("firstName");
+		expect(request.getParameter("LastName")).andReturn("");
+
+		replay(request);
+		boolean result = action.checkParameter(request);
+		verify(request);
+
+		assertEquals(false, result);
+	}
+
+	@Test
 	public void testExecuteNormal() {
 		SampleAction action = new SampleAction("firstName", "lastName");
 		HttpServletRequest request = createMock(HttpServletRequest.class);
